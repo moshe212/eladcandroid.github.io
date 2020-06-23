bc3ad771a74b936aaf9116fa524cfa9d64ca3859
eladcandroid <eladc.android@gmail.com>
2020-06-23 13:56:32 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,21 @@
+function useFetch(url, callback) {
+  fetch(url)
+    .then(function (res) {
+      return res.json();
+    })
+    .then(function (data) {
+      callback(data);
+    });
+}
+
+function callbackFunc(dataInFunc) {
+  const input = document.querySelector("input");
+  var value = input.value;
+  const resultDiv = document.querySelector(".result");
+  resultDiv.innerHTML = value * dataInFunc.rates.ILS;
+}
+
+var button = document.querySelector("button");
+button.onclick = function () {
+  useFetch("https://api.exchangeratesapi.io/latest", callbackFunc);
+};
